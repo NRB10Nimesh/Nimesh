a91c9dfd2f92870fccd764be5971fb13d52a4850
NIMESH Ranabhat <bhatrananimesh@gmail.com>
2024-08-02 19:25:11 +0545

    tenth commit

---
+++ lab1-Question-10-ans.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+  int a = 20;
+  printf("Program to print size required for different data type in C\n");
+  printf("Size of char is %d\n",sizeof(char));
+  printf("Size of int is %d\n",sizeof(int));
+  printf("Size of long is %d\n",sizeof(long));
+  printf("Size of float is %d\n",sizeof(float));
+  printf("Size of double is %d\n",sizeof(double));
+  printf("Size of long int is %d\n",sizeof(long int));
+  printf("Size of short int is %d\n",sizeof(short int));
+}
